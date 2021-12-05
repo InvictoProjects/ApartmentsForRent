705517fc488139d11d0660a5d8b4936d3e57844e
Pienskoi <vladymyrpenskoy@gmail.com>
2021-12-05 20:29:04 +0200

    Add creating details table script

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE TYPE building_type AS ENUM ('Brick', 'Frame', 'Silicate brick', 'Panel', 'Foam block', 'Monolith');
+
+CREATE TABLE descriptions (
+    id SERIAL PRIMARY KEY,
+    condition VARCHAR(50) NOT NULL,
+    type building_type NOT NULL,
+    additional_info TEXT
+);
+
+GRANT ALL PRIVILEGES ON TABLE descriptions to apartments_app;
