466bffbe5a2984909cc37b7a0e167d0d3dc3d691
Pienskoi <vladymyrpenskoy@gmail.com>
2021-12-05 20:37:28 +0200

    Add inserting test details

--- db/demo.sql
+++ db/demo.sql
@@ -18,3 +18,18 @@ VALUES ('Василь',
         'vasilpetrenko@gmail.com',
         '099 777 6537',
         'password3');
+
+INSERT INTO details (address, year, price, floor, quantity_of_rooms)
+VALUES ('Vinnytsia, Kosmonavtiv Ave, 43, apt. 83', '1965-01-01', 4000, 3, 2);
+
+INSERT INTO details (address, year, price, floor, quantity_of_rooms)
+VALUES ('Kyiv, Peremohy Street, 5, apt. 46', '1995-01-01', 4000, 3, 2);
+
+INSERT INTO details (address, year, price, floor, quantity_of_rooms)
+VALUES ('Zhytomyr, Chornoi Ave, 12, apt. 90', '1958-01-01', 1000, 5, 1);
+
+INSERT INTO details (address, year, price, floor, quantity_of_rooms)
+VALUES ('м. Київ, вул. Політехнічна, 41, кв. 316', '1945-01-01', 7000, 4, 1);
+
+INSERT INTO details (address, year, price, floor, quantity_of_rooms)
+VALUES ('м. Київ, вул. Банкова, 11, кв. 1', '1936-01-01', 1000000, 6, 10);
