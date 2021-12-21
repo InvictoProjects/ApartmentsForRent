c1408adf91bdfac72dcf4fe13aec431136f6f8f2
Koroliuk <koroliuk.yaroslav@gmail.com>
2021-12-22 00:08:36 +0200

    Reformat schema.sql and delete mvc client files

--- src/main/java/com/example/apartmentsforrent/persistence/dao/CrudDao.java
+++ src/main/java/com/example/apartmentsforrent/persistence/dao/CrudDao.java
@@ -3,10 +3,12 @@ package com.example.apartmentsforrent.persistence.dao;
 import java.util.Optional;
 
 public interface CrudDao<T, I> {
-
     T create(T entity);
+
     Optional<T> read(I id);
+
     void update(T entity);
+
     void delete(T entity);
 
 }
